7e89e2cb6d706a2d5e9010b51510f3fb4614dbcd
frogr <austindanielfrench@gmail.com>
2018-09-05 10:55:26 -0700

    refactored into separate functions

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const options = {
 };
 
 // clear the div so that it doesn't just keep scrolling content
-
 const emptyDiv = () => {
   const results = document.getElementsByClassName('results')[0];
   results.innerHTML = '';
@@ -24,40 +23,9 @@ const getResults = () => {
     if (this.status === 200) {
       var data = JSON.parse(this.response);
       for (let i = 0; i < 10; i++) {
-        const results = document.getElementsByClassName('results')[0];
-
-        const div = document.createElement('div');
-        div.className = 'resultItem';
-        results.append(div);
-
-        const title = data.items[i].snippet.title;
-        const channel = data.items[i].snippet.channelTitle;
-        const date = data.items[i].snippet.publishedAt
-          .toString()
-          .substring(0, 9);
-        const thumbnail = data.items[i].snippet.thumbnails.medium.url;
-        console.log(thumbnail);
-
-        const resultItem = document.getElementsByClassName('resultItem')[0];
-
-        const htmlTitle = document.createTextNode(`"${title}" `);
-        const htmlChannel = document.createTextNode(`by: ${channel} `);
-        const htmlDate = document.createTextNode(`published: ${date}`);
-        const img = document.createElement('img');
-        img.src = thumbnail;
-
-        results.append(div);
-        resultItem.appendChild(img);
-        resultItem.appendChild(document.createElement('br'));
-        resultItem.appendChild(htmlTitle);
-        resultItem.appendChild(document.createElement('br'));
-        resultItem.appendChild(htmlChannel);
-        resultItem.appendChild(document.createElement('br'));
-        resultItem.appendChild(htmlDate);
-        resultItem.appendChild(document.createElement('br'));
+        injectData(i, data);
       }
     } else {
-      console.log(this);
       console.log('failure after connecting');
     }
   };
@@ -71,6 +39,7 @@ const getResults = () => {
   const sortBy = document.getElementById('sort').value;
   options.order = sortBy;
 
+  // creating the URL that we're going to send to the youtube API by modifying the options object we created earlier, then sending it
   const REQUEST_URL =
     API_URL +
     'key=' +
@@ -87,7 +56,39 @@ const getResults = () => {
     '&' +
     'order=' +
     options.order;
-  console.log(REQUEST_URL);
   request.open('GET', REQUEST_URL, true);
   request.send();
+};
+
+// handling the data injection
+const injectData = (i, data) => {
+  const results = document.getElementsByClassName('results')[0];
+
+  const div = document.createElement('div');
+  div.className = 'resultItem';
+  results.append(div);
+
+  const title = data.items[i].snippet.title;
+  const channel = data.items[i].snippet.channelTitle;
+  const date = data.items[i].snippet.publishedAt.toString().substring(0, 9);
+  const thumbnail = data.items[i].snippet.thumbnails.medium.url;
+  console.log(thumbnail);
+
+  const resultItem = document.getElementsByClassName('resultItem')[0];
+
+  const htmlTitle = document.createTextNode(`"${title}" `);
+  const htmlChannel = document.createTextNode(`by: ${channel} `);
+  const htmlDate = document.createTextNode(`published: ${date}`);
+  const img = document.createElement('img');
+  img.src = thumbnail;
+
+  results.append(div);
+  resultItem.appendChild(img);
+  resultItem.appendChild(document.createElement('br'));
+  resultItem.appendChild(htmlTitle);
+  resultItem.appendChild(document.createElement('br'));
+  resultItem.appendChild(htmlChannel);
+  resultItem.appendChild(document.createElement('br'));
+  resultItem.appendChild(htmlDate);
+  resultItem.appendChild(document.createElement('br'));
 };
