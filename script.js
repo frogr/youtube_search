f565cc7e0bb9e2c51c4e32f6a911e9a27c23d69b
frogr <austindanielfrench@gmail.com>
2018-09-05 07:51:52 -0700

    have search working inside console

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-const key = 'AIzaSyBtw0iSiMB3XFpbiAmzODeQr5b-5UXDsPo';
-const url = 'https://www.googleapis.com/youtube/v3/search';
+const KEY = 'AIzaSyBtw0iSiMB3XFpbiAmzODeQr5b-5UXDsPo';
+const API_URL = 'https://www.googleapis.com/youtube/v3/search?';
 
 const options = {
-  type: '',
-  key: key,
-  q: 'surfing',
+  key: KEY,
+  q: '',
   maxResults: 25,
   part: 'snippet'
 };
 
 const getResults = () => {
   let request = new XMLHttpRequest();
-  request.open('GET', url, true);
 
   request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-      // Success!
+    if (this.status === 200) {
       var data = JSON.parse(this.response);
+      for (let i = 0; i <= 24; i++) {
+        console.log(
+          `title: ${data.items[i].snippet.title} by channel: ${
+            data.items[i].snippet.channelTitle
+          }`
+        );
+      }
     } else {
-      // We reached our target server, but it returned an error
+      console.log(this);
+      console.log('failure after connecting');
     }
   };
 
   request.onerror = function() {
-    // There was a connection error of some sort
+    console.log('failure to connect');
   };
 
+  const searchTerm = document.getElementById('searchTerm').value;
+  options.q = searchTerm;
+
+  const REQUEST_URL =
+    API_URL +
+    'key=' +
+    options.key +
+    '&' +
+    'q=' +
+    options.q +
+    '&' +
+    'part=' +
+    options.part +
+    '&' +
+    'maxResults=' +
+    options.maxResults;
+  request.open('GET', REQUEST_URL, true);
   request.send();
 };
